568d9c6a3a64f99e32c2a11c58f75539e3bc009d
Edenik <Edenik5@gmail.com>
2020-10-10 03:14:55 +0300

    conneting to firebase + fetching and adding data

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_chat_app/screens/chat_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Firebase Chat App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Starting position'),
-      ),
+      home: ChatScreen(),
     );
   }
 }
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/ssYMkkfdYetlKcDCXmGW/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: streamSnapshot.data.documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/ssYMkkfdYetlKcDCXmGW/messages')
+              .add({'text': 'New doc'});
+        },
+      ),
+    );
+  }
+}
