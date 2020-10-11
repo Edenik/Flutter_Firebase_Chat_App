eb0a8e0913ef2b6f2d6e102974e6b70b7e7a1d9f
Edenik <Edenik5@gmail.com>
2020-10-12 00:35:39 +0300

    preparing file upload & validating images

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+  final Function(File pickedImage) imagePickFn;
   @override
   _UserImagePickerState createState() => _UserImagePickerState();
 }
